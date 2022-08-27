7b19d78ebba301b9ebb888ddb7ee3d5270665241
isir10 <isir.malak@moringaschool.com>
2022-08-27 23:48:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function mondayWork(work="go to the office"){
     return`This Monday, I will ${work}.`;
 }
 const wrapAdjective = function (special = "*") {
-	return function (visual = "you are special") {
-		return `You are ${special}${visual}${special}!`;
+	return function (highlight = "a hard worker") {
+		return `You are ${special}${highlight}${special}!`;
 	};
 };
